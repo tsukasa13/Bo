bf3bab7079d0280845ae1cafa1b4e15415d370d4
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-08-19 10:53:08 +0000

    [Fix] 他のユーザーと自分を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update, :destroy ]
 
   def index
+    @users = User.all
     @book = Book.new
     @books = Book.all
     @user = current_user
